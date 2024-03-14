fdb1db0cd727fa4d3a3f2147d6a505999cc0e399
1Cezzo <emiljohnzen@gmail.com>
2024-03-14 20:42:54 +0100

    update selected player view

--- app/player/models/selected_player.py
+++ app/player/models/selected_player.py
@@ -1,13 +1,12 @@
 from django.db import models
-from django.utils import timezone
 from app.player.models.player import Player
 
 class SelectedPlayer(models.Model):
-    player = models.ForeignKey(Player, on_delete=models.CASCADE, null=True)
+    player = models.ForeignKey(Player, on_delete=models.CASCADE, blank=True, null=True)
     date = models.DateField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.player.name} - {self.date}"
+        return f"{self.player.name if self.player else 'Random Player'} - {self.date}"
     
     class Meta:
         db_table = 'selected_player'
